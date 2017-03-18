a17775c3c0a38ca8580ed29e52825ccf92b57d43
Ken Ellis <kenellis152@gmail.com>
2017-03-18 15:49:22 -0400

    added geolocation support via geolocation api

--- public/js/index.js
+++ public/js/index.js
@@ -3,12 +3,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  // socket.emit('createMessage', {
-  //   from: 'user1',
-  //   text: 'emitting createMessage upon connection to server'
-  // })
-
 });
 
 socket.on('newMessage', function (message) {
@@ -16,6 +10,16 @@ socket.on('newMessage', function (message) {
   var li = jQuery('<li></li>');
   li.text(`${message.from}: ${message.text}`);
 
+  jQuery('#messages').append(li);
+});
+
+socket.on('newLocationMessage', function (message) {
+  // console.log('received message from server: ', message);
+  var li = jQuery('<li></li>');
+  var a = jQuery('<a target="_blank">My current location</a>');
+  li.text(`${message.from}: `);
+  a.attr('href', message.url);
+  li.append(a);
   jQuery('#messages').append(li);
 });
 
@@ -33,3 +37,21 @@ jQuery('#message-form').on('submit', function (e) {
 
   });
 });
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function() {
+  if (!navigator.geolocation) {
+    return alert('Geolocation not supported by your browser');
+  }
+
+  navigator.geolocation.getCurrentPosition(function (position) {
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  }, function () {
+    alert('unable to fetch location.');
+  });
+});
+
+// https://www.google.com/maps?q=
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,5 @@
 var expect = require('expect');
-var {generateMessage} = require('./message.js');
+var {generateMessage, generateLocationMessage} = require('./message.js');
 
 describe('generateMessage', () => {
   it('should generate the correct message object', () => {
@@ -11,3 +11,17 @@ describe('generateMessage', () => {
     expect(output.createdAt).toBeA('number');
   });
 });
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    var from = "testuser123";
+    var latitude = 38.082817299999995;
+    var longitude = -84.5589946;
+    var output = generateLocationMessage(from, latitude, longitude);
+    expect(output.from).toBe(from);
+    expect(output.url).toBe(`https://www.google.com/maps?q=${latitude},${longitude}`);
+    expect(output.createdAt).toBeA('number');
+  });
+});
+
+// https://www.google.com/maps?q=38.082817299999995,-84.5589946

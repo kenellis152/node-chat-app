cf9670a05f32d486bbf6dcd74757d1e2c29467b5
Ken Ellis <kenellis152@gmail.com>
2017-03-17 21:31:16 -0400

    fixed heroku port bug

--- server/server.js
+++ server/server.js
@@ -4,13 +4,10 @@ const express = require('express');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000; // heroku port config
 
-console.log(__dirname + '/../public/html');
-console.log(publicPath);
-
 var app = express();
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log(`server started on port ${port}`);
 });
